c12306f780d56cf135f54c038c78a8abd9e96896
sean <15888897624@163.com>
2020-08-03 15:14:54 +0800

    BaseView

---
+++ app/src/main/java/com/test/mvpdemo/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.test.mvpdemo.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * desc   : BaseActivity
+ * Activity基类
+ */
+public class BaseActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/test/mvpdemo/base/BaseView.java
+++ app/src/main/java/com/test/mvpdemo/base/BaseView.java
@@ -1,6 +1,5 @@
 package com.test.mvpdemo.base;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
